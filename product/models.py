a1ed7f17cce01ac2d18a7610238c4d2bf9d5a6d8
arbuz93 <93.endo@gmail.com>
2017-05-02 18:29:07 +0200

    Create database

--- product/models.py
+++ product/models.py
@@ -2,41 +2,7 @@ from arbuz.models import *
 from user.models import User
 
 
-class Details(models.Model):
-
-    name = models.CharField(max_length=200)
-    description = models.TextField()
-
-    class Meta:
-        abstract = True
-
-    def __str__(self):
-        return self.name
-
-class Details_EN(Details):
-    pass
-
-class Details_PL(Details):
-    pass
-
-class Details_DE(Details):
-    pass
-
-
-
-class Where_Display(models.Model):
-
-    display_en = models.BooleanField()
-    display_pl = models.BooleanField()
-    display_de = models.BooleanField()
-
-    def __str__(self):
-        return 'en: {0}, pl: {1}, de: {2}'\
-            .format(self.display_en, self.display_pl, self.display_de)
-
-
-
-class Brand(models.Model):
+class Brand(Abstract_Model):
 
     name = models.CharField(max_length=20)
 
@@ -45,43 +11,69 @@ class Brand(models.Model):
 
 
 
-class Purpose(models.Model):
+class Widget(Abstract_Model):
 
-    name_en = models.CharField(max_length=20)
-    name_pl = models.CharField(max_length=20)
-    name_de = models.CharField(max_length=20)
+    name = models.CharField(max_length=50)
+    type = models.CharField(max_length=20)
 
     def __str__(self):
-        return self.name_en
+        return self.name
+
+
+
+class Values(Abstract_Model):
+
+    name = models.CharField(max_length=50)
+    super_price = models.IntegerField(default=0)
+    widget = models.ForeignKey(Widget)
+
+    def __str__(self):
+        return self.name
+
+
+
+class Catalog(Abstract_Model):
+
+    name = models.CharField(max_length=100)
+    image = models.ImageField(blank=True)
+    parent = models.ForeignKey('Catalog')
+
+    def Set_Variables(self):
+        self.image_dir = '/_static/img/catalog/'
+
+    def __str__(self):
+        return self.name
 
 
 
 class Product(Abstract_Model):
 
-    details_en = models.ForeignKey(Details_EN, null=True)
-    details_pl = models.ForeignKey(Details_PL, null=True)
-    details_de = models.ForeignKey(Details_DE, null=True)
-
-    price_eur = models.IntegerField(blank=True)
-    price_pln = models.IntegerField(blank=True)
-
+    name = models.CharField(max_length=100)
+    price = models.IntegerField(blank=True)
     image = models.ImageField(blank=True)
-    keywords = models.TextField(blank=True)
-    stock = models.IntegerField(blank=True)
-
-    where_display = models.ForeignKey(Where_Display)
     brand = models.ForeignKey(Brand, null=True, on_delete=models.SET_NULL)
-    purpose = models.ManyToManyField(Purpose)
+    parent = models.ForeignKey(Catalog)
 
     def Set_Variables(self):
         self.image_dir = '/_static/img/product/'
 
     def __str__(self):
-        return self.details_en.name
+        return self.name
 
 
 
-class Recommended_Product(models.Model):
+class Subproduct(Abstract_Model):
+
+    unique = models.CharField(max_length=20)
+    produkt = models.ForeignKey(Product)
+    values = models.ManyToManyField(Values)
+
+    def __str__(self):
+        return self.unique
+
+
+
+class Recommended_Product(Abstract_Model):
 
     product = models.ForeignKey(Product)
 
@@ -90,7 +82,7 @@ class Recommended_Product(models.Model):
 
 
 
-class Favorite_Product(models.Model):
+class Favorite_Product(Abstract_Model):
 
     product = models.ForeignKey(Product)
     user = models.ForeignKey(User)
