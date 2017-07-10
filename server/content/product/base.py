b559338f5f2c32ea02aadfda9c2fea719c4297a6
arbuz93 <93.endo@gmail.com>
2017-07-10 11:45:09 +0200

    Fixed: setting, tags, product.base

--- server/content/product/base.py
+++ server/content/product/base.py
@@ -3,42 +3,6 @@ from server.manage.switch.base import *
 
 class Product_Models_Manager(Dynamic_Base):
 
-    def Get_Product_Name(self, product):
-
-        # if self.request.session['translator_language'] == 'EN':
-        #     return product.details_en.name
-        #
-        # if self.request.session['translator_language'] == 'PL':
-        #     return product.details_pl.name
-        #
-        # if self.request.session['translator_language'] == 'DE':
-        #     return product.details_de.name
-        return ''
-
-    def Get_Product_Description(self, product):
-
-        # if self.request.session['translator_language'] == 'EN':
-        #     return product.details_en.description
-        #
-        # if self.request.session['translator_language'] == 'PL':
-        #     return product.details_pl.description
-        #
-        # if self.request.session['translator_language'] == 'DE':
-        #     return product.details_de.description
-        return ''
-
-    def Get_Purpose_Name(self, purpose):
-
-        # if self.request.session['translator_language'] == 'EN':
-        #     return purpose.name_en
-        #
-        # if self.request.session['translator_language'] == 'PL':
-        #     return purpose.name_pl
-        #
-        # if self.request.session['translator_language'] == 'DE':
-        #     return purpose.name_de
-        return ''
-
     def Get_Product_Price(self, product, currency=None,
                           current_currency=False):
 
--- server/manage/setting/views.py
+++ server/manage/setting/views.py
@@ -12,32 +12,19 @@ class Control_Panel(Dynamic_Event_Manager):
         return self.Render_HTML('setting/control_panel.html')
 
     @staticmethod
-    def Manage_Button_Reset_Database():
-        os.system(BASE_DIR + '/reset.sh')
-        return JsonResponse({'__button__': 'true'})
-
-    def Manage_Button_Load_Product(self):
-        self.Manage_Button_Reset_Database()
-        cosmetix.Start()
-        return JsonResponse({'__button__': 'true'})
-
-    @staticmethod
     def Manage_Button_Reset_Passwords():
+
         user = User.objects.get(username='Drego31')
         user.password = Dynamic_Base.Encrypt('kaktus88')
         user.save()
+
         root = Root.objects.first()
         root.password = Dynamic_Base.Encrypt('kaktus88')
         root.save()
+
         return JsonResponse({'__button__': 'true'})
 
     def Manage_Button(self):
-
-        if self.request.POST['__button__'] == 'reset_databases':
-            return self.Manage_Button_Reset_Database()
-
-        if self.request.POST['__button__'] == 'load_product':
-            return self.Manage_Button_Load_Product()
 
         if self.request.POST['__button__'] == 'reset_passwords':
             return self.Manage_Button_Reset_Passwords()
@@ -46,5 +33,5 @@ class Control_Panel(Dynamic_Event_Manager):
 
     @staticmethod
     def Launch(request):
-        return Control_Panel(request).HTML
+        return Control_Panel(request, only_root=True).HTML
 
