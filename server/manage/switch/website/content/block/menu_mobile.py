5307619b2b24d568466d594dbeb6e0302ad54d53
arbuz93 <93.endo@gmail.com>
2017-07-21 21:32:42 +0200

    Fixed: bug lasts commits

--- server/manage/switch/website/content/block/menu_mobile.py
+++ server/manage/switch/website/content/block/menu_mobile.py
@@ -4,8 +4,8 @@ from server.manage.switch.website.endpoints import *
 class Menu_Mobile_Block(Endpoints):
 
     def Manage_Content(self):
-        return self.Render_HTML('block/menu_mobile.html')
+        return self.Render_HTML('block/menu.html')
 
     def Error(self, response_class, context):
         return response_class(self.Render_To_String(
-            'error/menu_mobile.html', context=context))
+            'error/menu.html', context=context))
