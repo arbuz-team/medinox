bbbc512babdd259203069f52a620d978935cd6be
arbuz93 <93.endo@gmail.com>
2017-08-26 01:04:05 +0200

    Fixed: if last_notification_pk is not defined, then return first ten records

--- server/service/notification/views.py
+++ server/service/notification/views.py
@@ -5,12 +5,20 @@ from server.service.notification.models import *
 class Notification_Manager(Website_Manager):
 
     def Manage_Get_Next_Ten_Notifications(self):
-
         last_notification_pk = self.request.POST['last_notification_pk']
-        date = SQL.Get(Notification_Model, pk=last_notification_pk).date
 
-        self.context['notifications'] = SQL.Filter(
-            Notification_Model, date__lt=date).order_by('-date')[:10]
+        # when not defined, then first records
+        if not last_notification_pk:
+
+            self.context['notifications'] = SQL.Filter(
+                Notification_Model).order_by('-date')[:10]
+
+        else:
+
+            # records after date from last_notification_pk
+            date = SQL.Get(Notification_Model, pk=last_notification_pk).date
+            self.context['notifications'] = SQL.Filter(
+                Notification_Model, date__lt=date).order_by('-date')[:10]
 
         return self.Render_HTML('notification/list.html')
 
