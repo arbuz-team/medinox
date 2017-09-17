aa40a8e75d9e43f42ea36e1bc410d6629a49782c
arbuz93 <93.endo@gmail.com>
2017-09-17 17:39:07 +0200

    Added: NIP/TIN number to abstract address

--- server/manage/switch/forms/address.py
+++ server/manage/switch/forms/address.py
@@ -5,6 +5,7 @@ class Abstract_Address_Form(Abstract_Model_Form):
 
     def Create_Fields(self):
         self.fields['country'] = forms.ChoiceField(choices=countries)
+        self.fields['nip'] = forms.CharField(max_length=20, required=False)
 
     def Set_Widgets(self):
 
@@ -13,6 +14,7 @@ class Abstract_Address_Form(Abstract_Model_Form):
         city_attr = self.Attr(Text(self, 50), classes='test', autocomplite='')
         region_attr = self.Attr(Text(self, 51), classes='test', autocomplite='')
         postcode_attr = self.Attr(Text(self, 52), classes='test', autocomplite='')
+        nip_attr = self.Attr(Text(self, 199), classes='test', autocomplite='')
         country_attr = self.Attr(field=Field.SELECT, autocomplite='')
 
         self.fields['full_name'].widget = forms.TextInput(attrs=full_name_attr)
@@ -20,6 +22,7 @@ class Abstract_Address_Form(Abstract_Model_Form):
         self.fields['city'].widget = forms.TextInput(attrs=city_attr)
         self.fields['region'].widget = forms.TextInput(attrs=region_attr)
         self.fields['postcode'].widget = forms.TextInput(attrs=postcode_attr)
+        self.fields['nip'].widget = forms.TextInput(attrs=nip_attr)
         self.fields['country'].widget.attrs = country_attr
 
     def Exclude_Fields(self):
--- server/manage/switch/models/additional.py
+++ server/manage/switch/models/additional.py
@@ -9,6 +9,7 @@ class Abstract_Address(Abstract_Model):
     city = models.CharField(max_length=50)
     region = models.CharField(max_length=50)  # state/province/region
     postcode = models.CharField(max_length=10)  # zip/postal code
+    nip = models.CharField(max_length=20)
     country = CountryField()
 
     class Meta:
