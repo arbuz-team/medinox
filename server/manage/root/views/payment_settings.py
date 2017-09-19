7d5ef7603166d57c60522e7e3553f5c9e63df30f
arbuz93 <93.endo@gmail.com>
2017-09-19 20:39:14 +0200

    Added: payment settings page in root panel V2

---
+++ server/manage/root/views/payment_settings.py
@@ -0,0 +1,29 @@
+from server.manage.switch.website.manager import *
+from server.service.payment.forms import *
+
+
+class Payment_Settings(Website_Manager):
+
+    def Manage_Content(self):
+        self.context['delivery'] = SQL.First(Model_Delivery)
+        return self.Render_HTML('root/payment_settings.html')
+
+    def Manage_Form(self):
+
+        if self.request.POST['_name_'] == 'delivery_settings':
+            delivery = SQL.First(Model_Delivery)
+
+            if self.request.POST['price_on_receipt']:
+                delivery.price_on_receipt = \
+                    self.request.POST['price_on_receipt']
+
+            if self.request.POST['price_in_advance']:
+                delivery.price_on_receipt = \
+                    self.request.POST['price_in_advance']
+
+            SQL.Save(data=delivery)
+            return HttpResponse()
+
+    @staticmethod
+    def Launch(request):
+        return Payment_Settings(request, only_root=True).HTML
