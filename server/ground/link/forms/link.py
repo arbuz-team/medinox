aded714d530e3d121a5a260e3089946d0d5cc0d8
arbuz93 <93.endo@gmail.com>
2017-10-04 23:28:10 +0200

    Fixed: link form

--- server/ground/link/forms/link.py
+++ server/ground/link/forms/link.py
@@ -7,12 +7,15 @@ class Form_Link(Abstract_Form):
     def clean(self):
 
         link = self.request.session['link_editing']
-        if 'target' not in self.data:
+        language = self.request.session['translator_language']
+        target_name = 'target_' + language.lower()
+
+        if target_name not in self.data:
             raise forms.ValidationError(Text(self, 189))
 
         # get data
         name = str(link)
-        parent = SQL.Get(Model_Catalog, pk=self.data['target'])
+        parent = SQL.Get(Model_Catalog, pk=self.data[target_name])
         children = SQL.Filter(Model_Catalog, parent=parent)
 
         # check if catalog with the name exists
