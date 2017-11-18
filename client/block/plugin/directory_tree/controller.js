eb91cb4940a2e03e865b4f0a749eff7fef0e860d
Drego31 <dominik.betka@arbuz.team>
2017-11-18 16:49:35 +0100

    Fixed: display arrow in catalog's link tree.

--- client/block/plugin/directory_tree/controller.js
+++ client/block/plugin/directory_tree/controller.js
@@ -19,14 +19,23 @@ export function Directory_Tree(container)
 	{
 		let
 			$parent = $(this).parent().parent(),
+			$arrow = $parent.children('.directory_tree-single').children('.directory_tree-name').children('.directory_tree-arrow'),
 			$list = $parent.children('.directory_tree-list'),
 			$all_lists = $parent.find('.directory_tree-list'),
 			is_open = $list.hasClass('is-open');
 
 		if(is_open)
+		{
+			if($arrow.hasClass('is-open'))
+				$arrow.removeClass('is-open').addClass('is-close');
 			$all_lists.removeClass('is-open').addClass('is-close');
+		}
 		else
+		{
+			if($arrow.hasClass('is-close'))
+				$arrow.removeClass('is-close').addClass('is-open');
 			$list.removeClass('is-close').addClass('is-open');
+		}
 	};
 
 
--- client/static/compress/bundle.js
+++ client/static/compress/bundle.js
@@ -4372,11 +4372,18 @@
 	
 		var change_state = function change_state() {
 			var $parent = $(this).parent().parent(),
+			    $arrow = $parent.children('.directory_tree-single').children('.directory_tree-name').children('.directory_tree-arrow'),
 			    $list = $parent.children('.directory_tree-list'),
 			    $all_lists = $parent.find('.directory_tree-list'),
 			    is_open = $list.hasClass('is-open');
 	
-			if (is_open) $all_lists.removeClass('is-open').addClass('is-close');else $list.removeClass('is-close').addClass('is-open');
+			if (is_open) {
+				if ($arrow.hasClass('is-open')) $arrow.removeClass('is-open').addClass('is-close');
+				$all_lists.removeClass('is-open').addClass('is-close');
+			} else {
+				if ($arrow.hasClass('is-close')) $arrow.removeClass('is-close').addClass('is-open');
+				$list.removeClass('is-close').addClass('is-open');
+			}
 		};
 	
 		this.define = function () {
